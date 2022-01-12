1ef14e64334211427430ab7a7c08d8346b2aae9d
MatveyYakimov <90453661+MatveyYakimov@users.noreply.github.com>
2022-01-13 02:08:07 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,4 @@
+surname = (str(input("Введи фамилию ")))
+name = (str(input("Введи имя ")))
+patronymic = (str(input("Введи отчество ")))
+print("ФИО = ",(surname[0]+"."),(name[0]+"."),(patronymic[0]+"."))
---
+++ tasl 6.py
@@ -0,0 +1,6 @@
+print('Напишите предложение:')
+string = input()
+f = {}
+for i in string:
+    f[i] = f.get(i, 0) + 1
+print(f)
