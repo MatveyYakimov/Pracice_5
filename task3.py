b0dbcbdb29ba946def7bed1f93e7b14dae541c09
MatveyYakimov <90453661+MatveyYakimov@users.noreply.github.com>
2022-01-13 02:10:33 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,4 +1,4 @@
-surname = (str(input("Введи фамилию ")))
-name = (str(input("Введи имя ")))
-patronymic = (str(input("Введи отчество ")))
-print("ФИО = ",(surname[0]+"."),(name[0]+"."),(patronymic[0]+"."))
+surname = (str(input("Введите фамилию ")))
+name = (str(input("Введите имя ")))
+patronymic = (str(input("Введите отчество ")))
+print("Ваше ФИО = ",(surname[0]+"."),(name[0]+"."),(patronymic[0]+"."))
